076389c81ddffe9d0c41f15f4f7a9826a598d667
Regina Casias <reginaannvigil@gmail.com>
2021-03-31 14:27:46 -0600

    Started testing for dealership

---
+++ lib/dealership.rb
@@ -0,0 +1,12 @@
+class Dealership
+  attr_reader :name,
+              :address,
+              :inventory,
+              :inventory_count
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @inventory = []
+    @inventory_count = 0
+  end
+end
