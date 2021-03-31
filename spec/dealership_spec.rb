c19dcb648fdab34d9343cabf955e37aca4ed0d23
Regina Casias <reginaannvigil@gmail.com>
2021-03-31 14:36:16 -0600

    Added testing for make category

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -29,5 +29,32 @@ RSpec.describe Car do
       expect(dealership.inventory).to eq([car_1, car_2])
       expect(dealership.inventory_count).to eq(2)
     end
+
+    it 'can track inventory' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+      expect(dealership.has_inventory?).to eq(false)
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+
+      expect(dealership.has_inventory?).to eq(true)
+    end
+
+    it 'can find cars by category' do
+      dealership = Dealership.new("Acme Auto", "123 Main Street")
+      car_1 = Car.new("Ford Mustang", 1500, 36)
+      car_2 = Car.new("Toyota Prius", 1000, 48)
+      car_3 = Car.new("Toyota Tercel", 500, 48)
+      car_4 = Car.new("Chevrolet Bronco", 1250, 24)
+      dealership.add_car(car_1)
+      dealership.add_car(car_2)
+      dealership.add_car(car_3)
+      dealership.add_car(car_4)
+
+      expect(dealership.dealership.cars_by_make("Toyota")).to eq([car_2, car_3])
+      expect(dealership.dealership.cars_by_make("Toyota")).to eq([car_1])
+    end
   end
 end
